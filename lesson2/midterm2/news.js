132bcf0186614f3f0dacdb043051c2ae14ed90d5
hieujr <hieu.nguyenvan@bluebottle.digital>
2023-04-05 18:52:05 +0700

    init bundles branch

---
+++ lesson2/midterm2/news.js
@@ -0,0 +1,44 @@
+{
+   "news": [
+         {
+               "title": "Đề xuất đầu tư hơn 94.000 tỷ đồng phát triển hạ tầng miền Tây",
+               "content": "16 dự án xây dựng đường ven biển, kết nối vùng, đê bao chống sạt lở, hồ trữ nước ngọt… được đề xuất đầu tư ở miền Tây với tổng vốn hơn 94.300 tỷ đồng.Thông tin được Bộ Kế hoạch và Đầu tư công bố tại cuộc họp lần thứ nhất của Ban Chỉ đạo các dự án phát triển bền vững vùng Đồng bằng sông Cửu Long ứng phó với biến đổi khí hậu vừa diễn ra tại Cần Thơ.Các dự án đã được hai bộ Giao thông Vận tải, Nông nghiệp và Phát triển Nông thôn cùng 13 tỉnh, thành miền Tây thống nhất đề xuất. Trong đó, vốn vay nước ngoài hơn 2,8 tỷ USD (tương đương 66.282 tỷ đồng), vốn đối ứng hơn 28.000 tỷ đồng."
+         },
+         {
+               "title": "Newcastle giành vị trí thứ ba từ Man Utd",
+               "content": "Bất bại bốn trận gần nhất, trong đó có ba chiến thắng, nhưng Man Utd được dự đoán gặp nhiều khó khăn khi làm khách của Newcastle - đội thường xuyên có mặt trong nhóm đua tranh top 4 mùa này. Và 90 phút trên sân St James' Park chứng tỏ điều đó. Đội chủ nhà áp đảo với 15 lần dứt điểm, trong đó có sáu pha trúng đích. Còn Man Utd chỉ dứt điểm bốn lần, với vỏn vẹn một cú sút trúng khung thành từ Antony"
+         },
+         {
+               "title": "Chứng khoán sẽ thế nào sau chuỗi tăng dài nhất 20 tháng",
+               "content": "Chứng khoán sẽ thế nào sau chuỗi tăng dài nhất 20 tháng. Nhiều chuyên gia cho rằng thị trường chứng khoán sẽ sớm đứt mạch tăng bởi nhà đầu tư có nhu cầu hiện thực hóa lợi nhuận sau 9 phiên đi lên liên tiếp. VN-Index đang có chuỗi tăng dài nhất kể từ cuối tháng 7/2021 với 9 phiên xanh liên tiếp. Chỉ số đại diện cho sàn TP HCM đã tích lũy 32 điểm trong chuỗi này, lên mức 1.064 điểm - vùng giá được nhiều công ty chứng khoán đánh giá là thử thách để chuyển đổi xu hướng hiện tại từ đi ngang thành tăng giá. Ông Đinh Quang Hinh, Trưởng bộ phận Vĩ mô và chiến lược thị trường của Công ty Chứng khoán VNDirect, nhìn nhận VN-Index đã tăng liên tiếp trong sự nghi ngờ và thận trọng của nhà đầu tư cá nhân. Điều này thể hiện qua việc biên độ mỗi phiên chỉ khoảng 2-9 điểm, hoặc nếu tính theo giá trị tương đối thì không phiên nào trong chuỗi này tăng hơn 1%. Ngoài ra, thanh khoản thị trường cũng cải thiện không đáng kể khi phiên thấp nhất chưa đến 8.000 tỷ đồng và cao nhất không quá 12.000 tỷ đồng."
+         },
+         {
+            "title": "Chelsea sa thải HLV Potter",
+            "content": "Potter nhất trí hợp tác với CLB để chuyển giao nhanh gọn. Trong thời gian ông làm việc, Potter đã đưa Chelsea vào tứ kết Champions League, nơi đội sẽ gặp Real Madrid. Chelsea cảm ơn đóng góp và nỗ lực của Potter, chúc ông thành công trong tương lai\", thông báo của Chelsea có đoạn. Cũng trong thông báo, hai đồng chủ sở hữu Chelsea - Todd Boehly, Behdad Eghbali - nhấn mạnh họ \"tôn trọng tài năng, phong thái chuyên nghiệp của Potter\", và các bên \"đều thất vọng với kết cục này"
+         },
+         {
+            "title": "Thủ khoa Ngoại thương tốt nghiệp sớm với điểm gần tuyệt đối",
+            "content": "Trần Thị Thu Hiền là thủ khoa tốt nghiệp sớm của trường Đại học Ngoại thương với điểm trung bình 3,99/4, chỉ duy nhất một môn không đạt điểm A. Nữ sinh quê Can Lộc, Hà Tĩnh, đặt mục tiêu tốt nghiệp trong 3,5 năm từ ngày mới nhập học ngành Kinh tế đối ngoại. Thu Hiền nhìn nhận việc ra trường sớm có lợi hơn khi tỷ lệ cạnh tranh xin việc làm không cao như thời điểm sinh viên tốt nghiệp đồng loạt. Đại diện cho hơn 1.300 sinh viên phát biểu trong lễ tốt nghiệp hôm 2/4, cô gái 22 tuổi nói đây là thành quả của sự nỗ lực, cố gắng liên tục. Nữ sinh cho biết không đặt mục tiêu tốt nghiệp thủ khoa hay bảng điểm toàn điểm A mà chỉ xác định làm tốt nhất trong mọi hoàn cảnh."
+         },
+         {
+            "title": "Taylor Swift - cỗ máy kiếm tiền",
+            "content": "Taylor Swift được dự đoán có thể trở thành tỷ phú sau khi chuyến lưu diễn mới nhất của cô kết thúc vào tháng 9. Ca sĩ đang thực hiện The Eras Tour với 52 buổi diễn vòng quanh thế giới. Đêm đầu tiên tại thành phố Glendale, bang Arizona hôm 17/3 có 69.000 người tham dự. Con số này giúp Taylor Swift trở thành nữ nghệ sĩ có lượng khán giả tham dự concert mở màn cao nhất nước Mỹ. Qua những buổi diễn ở Las Vegas, Arlington, Taylor đưa người hâm mộ trải nghiệm từ ngạc nhiên này đến ngạc nhiên khác. Ca sĩ hát live 44 ca khúc trong ba tiếng, mỗi tiết mục đều được dàn dựng công phu."
+         },
+         {
+            "title": "Lầu Năm Góc loay hoay giải thích diễn biến vụ bắn hạ khí cầu Trung Quốc",
+            "content": "Giới lãnh đạo quân sự Mỹ đưa ra lời kể bất nhất về lý do họ không lập tức bắn hạ khí cầu Trung Quốc hồi cuối tháng 1, khiến phe Cộng hòa chỉ trích chính quyền Biden. Mỹ hôm 4/2 triển khai tiêm kích bắn hạ khí cầu Trung Quốc vì cho rằng đây là khí cầu do thám, bất chấp Bắc Kinh giải thích đó là khí cầu dân sự phục vụ mục đích nghiên cứu khí tượng bay lạc vào lãnh thổ Mỹ. Phe Cộng hòa và cả một số nghị sĩ Dân chủ đã chỉ trích chính quyền Biden phản ứng chậm khi vụ bắn hạ được thực hiện 7 ngày sau khi khí cầu vào lãnh thổ Mỹ. Những lời chỉ trích càng gia tăng khi các lãnh đạo quân sự Mỹ tuần trước đưa ra lời kể bất nhất về lý do họ không bắn hạ ngay khí cầu. Glen VanHerck, người đứng đầu Bộ tư lệnh Phương Bắc quân đội Mỹ (NORTHCOM) cho biết Bộ trưởng Quốc phòng Lloyd Austin đã không yêu cầu ông đưa ra đề xuất phản ứng cho đến vài ngày sau khi khí cầu Trung Quốc bay vào Mỹ. Trong khi đó, phát ngôn viên của ông Austin nói rằng ngay từ đầu sự việc ông đã yêu cầu VanHerck đưa ra khuyến nghị nhưng tướng này không nêu phương án bắn hạ."
+         },
+         {
+            "title": "Mercedes GLS 2024 lộ diện",
+            "content": "Mẫu SUV hạng sang bản nâng cấp có kiểu dáng mới, những thay đổi về phần mềm và thiết bị mới, xe ra mắt ngày 4/4. Mercedes tung ảnh hé lộ GLS-class 2024 trước ngày ra mắt chính thức. Theo chia sẻ của hãng, GLS 2024 là bản facelift (nâng cấp giữa vòng đời), sẽ có diện mạo mới với lưới tản nhiệt thiết kế mới, cụm đèn trước vẫn như bản cũ, trong khi đèn hậu vẫn là một ẩn số."
+         },
+         {
+            "title": "Giá dầu thế giới tăng vọt vì OPEC+ bất ngờ giảm sản xuất",
+            "content": "Dầu WTI sáng nay tăng 8% - mạnh nhất hơn một năm, khi Tổ chức Các nước Xuất khẩu Dầu mỏ và đồng minh (OPEC+) đột ngột thông báo giảm sản xuất. Giá dầu thô Mỹ WTI sáng nay có thời điểm chạm 80,89 USD một thùng. Dầu Brent cũng lên 85,54 USD. Hiện tại, hai loại dầu trên có giá lần lượt 80,2 USD và 84,7 USD. Hôm 2/4, hàng loạt quốc gia trong OPEC+ thông báo tự nguyện giảm sản xuất từ tháng tới, với mức tổng hơn 1 triệu thùng mỗi ngày. Trong đó, Arab Saudi sẽ giảm 500.000 thùng một ngày từ tháng 5/2022 đến hết năm 2023. Phó thủ tướng Nga cũng cho biết Moskva sẽ gia hạn việc giảm sản xuất 500.000 thùng một ngày đến hết năm sau. UAE, Kuwait, Iraq, Oman và Algeria cũng tình nguyện giảm sản xuất 40.000 - 211.000 thùng trong thời gian này."
+         },
+         {
+            "title": "Bốn cách ôn luyện từ thầy giáo 5 lần đạt 9.0 IELTS",
+            "content": "Theo chuyên gia luyện thi IELTS, kỹ năng hấp thụ như Nghe và Đọc cần được tập trung nhiều hơn để đạt được hiệu quả tốt nhất. Đặng Trần Tùng, người Việt Nam đầu tiên 5 lần đạt 9.0 IELTS, cho biết tất cả bài thi kiểm tra trình độ tiếng Anh quốc tế như IELTS, PTE, TOEFL đều được quy về Khung tham chiếu châu Âu (CEFR) với thang đánh giá từ A1 đến C2. Theo một nghiên cứu của Đại học Cambridge, người học mất trung bình 200 giờ để lên được một bậc trong khung tham chiếu này. Nếu xuất phát từ A1, bạn phải leo mất ba bậc mới lên được B2, tức cần 600 giờ học có người hướng dẫn. Tuy nhiên, \"học đến cấp ba mà bạn vẫn ở mức bắt đầu (A1) thì để đạt được B2 trong 600 giờ là điều gần như không thể\", thầy giáo 9X nhận định. Theo Đặng Trần Tùng, có những phương pháp để người học rút ngắn được lộ trình, nhưng nếu đó là mẹo hay đường tắt mà không cải thiện trình độ tiếng Anh thực chất thì không nên theo."
+         },
+   ]
+}
